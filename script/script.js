6eaf53fbfe31bf55333d63ec1b75a8190682dcbe
Igor_82158215 <igor.chepurnyi@gmail.com>
2021-12-21 18:49:00 +0200

    new commit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,25 @@
-$("#myBtn").click(function(){
-  $("#demo2").carousel("prev");
-});
-$("#myBtn1").click(function(){
-  $("#demo2").carousel("next");
-});
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
